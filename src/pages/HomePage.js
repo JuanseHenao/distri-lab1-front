dc7352eb0fd444d28300d7cc4312d3044cea7504
JuanseHenao <juanse41601@gmail.com>
2023-07-19 01:41:30 -0500

    fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -40,7 +40,7 @@ function HomePage() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://18.228.43.72:5000", config);
+      const response = await axios.get("http://localhost:5000", config);
       console.log(response);
       setData(response.data);
       /* setData([
