de7d11ccc23f076d2f4d737e8562823c8eea77df
JuanseHenao <juanse41601@gmail.com>
2023-07-18 23:38:16 -0500

    App js changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./index.css"; // Importa tu archivo CSS personalizado aquí
 import HomePage from "./pages/HomePage";
-
-// Componentes para las diferentes páginas
-
-const AboutPage = () => (
-  <div>
-    <h1>Acerca de</h1>
-    <p>Información sobre nosotros</p>
-  </div>
-);
-
-const ContactPage = () => (
-  <div>
-    <h1>Contacto</h1>
-    <p>Información de contacto</p>
-  </div>
-);
 
 function App() {
   return <HomePage />;
